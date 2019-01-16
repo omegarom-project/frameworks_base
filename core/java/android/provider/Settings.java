4b4a5bc84f5edd594635570179a828aee3df54b7
neobuddy89 <neobuddy89@gmail.com>
2019-01-16 08:04:37 +0530

    PebbleBerry: Introduce Tile Styles - a QS tile style picker [1/3]
    This gives users the chance to change the look of their QS tiles. Being based on the same methods used by Omni's system theming and DU's Fruity Pebbles, it seems to work quite nicely with them.
    
    Originally implemented by calebcabob
    Code Ref: https://github.com/AquariOS/frameworks_base/commit/7d82a97d78c96a13fee931e634633f6ace6bac82
    
    Re-write from scratch.
    
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4229,6 +4229,12 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Switch between different qs tile styles
+         * @hide
+         */
+        public static final String BERRY_QS_TILE_STYLE = "berry_qs_tile_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
